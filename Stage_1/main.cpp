67146d33277af3cb88f0d718d696a1b6bf574435
DemonXD <kanonxmm@163.com>
2021-01-05 15:41:18 +0800

    init

---
+++ Stage_1/main.cpp
@@ -0,0 +1,17 @@
+#include "headfile/baseConcept.hpp"
+
+
+int main()
+{
+    VARS();
+    CONDITIONALOPERATOR();
+    ARRAY();
+    ARRAY2D();
+    // VECTOR(); // need user input
+    ITERATOR();
+    MODULUS();
+    FPPBPP();
+    //###########################################
+    // std::cout << std::fixed << std::setprecision(2) << 1.2587 << std::endl; // from  include <iomanip>
+    return 0;
+}
